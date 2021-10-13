5b4e050d831f0f0a62601f0c12498865b64a8c77
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2021-10-13 23:32:35 +0300

    Create hello.c

---
+++ beginner/hello.c
@@ -0,0 +1,12 @@
+/* Let's start with "Hello World" as always */
+
+
+#include <stdio.h> /*1*/
+main() /*2*/
+{
+  printf("Hello world!\n"); /*3*/
+}
+
+/* 1-include information about standard library*/
+/* 2-main is a function, receives no argument values*/
+/* 3-\n = the newline character*/
