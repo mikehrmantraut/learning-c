b56d52f8f99e632107f16929b52ba1cab1b40c01
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2021-10-28 09:15:08 +0300

    Update find-bigger-num-btw-2.c

--- beginner/find-bigger-num-btw-2.c
+++ beginner/find-bigger-num-btw-2.c
@@ -1,1 +1,29 @@
+/**
+ * C program to find bigger number between two numbers.
+ */
+#include <stdio.h>
 
+int main()
+{
+	int num1, num2;
+	
+	/* Input first number from user */
+	printf("Please enter first number.");
+	scanf("%d",&num1);
+	
+	/* Input second number from user */
+	printf("Please enter second number.");
+	scanf("%d",&num2);
+	
+	/* if first number is bigger */
+	if (num1 > num2)
+		printf("First number (%d) is bigger than second number (%d).", num1, num2);
+	/* if second number is bigger */
+	else if (num2 > num1)
+		printf("Second number (%d) is bigger than first number (%d).", num2, num1);
+	/* check for equality */ 
+	else
+		printf("These numbers are equal.");
+	
+	return 0;
+}
