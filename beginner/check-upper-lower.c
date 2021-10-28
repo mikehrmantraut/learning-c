f6a5d55b3fe3485f5bfb5712acb3c6f92b561011
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2021-10-28 19:51:30 +0300

    Create check-upper-lower.c

---
+++ beginner/check-upper-lower.c
@@ -0,0 +1,27 @@
+/**
+* C program to check whether a character is Uppercase or Lowercase.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	char ch;
+	
+	/* input character from user */
+	printf("Please enter any letter.");
+	scanf("%c", &ch);
+	
+	/* checking lowercase */
+	if (ch>='a' && ch <= 'z')
+		printf("%c is LOWERCASE", ch);
+	
+	/* checking uppercase */
+	else if (ch>='A' && ch <= 'Z')
+		printf("%c is UPPERCASE", ch);
+	
+	else
+		printf("%c is not alphabet ", ch);
+	
+	return 0;
+}
