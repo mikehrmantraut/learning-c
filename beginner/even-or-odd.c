be3922e71270af5089ff35e35b5cfe977bfcabb5
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2021-10-28 11:59:41 +0300

    Create even-or-odd.c

---
+++ beginner/even-or-odd.c
@@ -0,0 +1,22 @@
+/*
+** C program check whether a number is even or odd.
+*/
+
+#include <stdio.h>
+int main()
+{
+	int num;
+	
+	printf("Please enter a number: ");
+	scanf("%d", &num);
+	
+	/* if number can divided by 2 than its even */
+	if(num%2==0)
+		printf("%d is even number.", num);
+	
+	/* if number can not divided by 2 than its odd */
+	else
+		printf("%d is odd number.", num);
+	
+  return 0;
+}
