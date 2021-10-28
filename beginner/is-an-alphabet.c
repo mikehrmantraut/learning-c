a7879c81dab09cf6e90c07741cc04ba56cd0605a
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2021-10-28 13:05:52 +0300

    Create is-an-alphabet.c

---
+++ beginner/is-an-alphabet.c
@@ -0,0 +1,21 @@
+/**
+* C program whether first character is alphabet or not.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	char ch;
+	
+	/* Input a character from user */
+	printf("Please enter any character.");
+	scanf("%c", &ch);
+	
+	if ((ch>='a' && ch <='z') || (ch>='A' && ch<='Z'))
+		printf("Character is an ALPHABET.");
+	else
+		printf("Character is NOT ALPHABET.");
+	
+	return 0;	
+}
