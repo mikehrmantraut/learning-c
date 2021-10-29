da8c3d6ec9b68e03854a6d1250890f7df1ba031c
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2021-10-29 09:46:49 +0300

    Update divisible-number.c

--- beginner/divisible-number.c
+++ beginner/divisible-number.c
@@ -1,6 +1,6 @@
-/*
-** C program to check whether a number is divisible by 5 and 11 or not.
-*/
+/**
+ * C program to check whether a number is divisible by 5 and 11 or not.
+ */
 
 #include <stdio.h>
 
