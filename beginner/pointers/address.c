7d9e47f4ffc8f93ae86de67ca00c8317c169bea2
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-08-02 19:29:54 +0300

    Update address.c

--- beginner/pointers/address.c
+++ beginner/pointers/address.c
@@ -5,12 +5,12 @@ int main()
     char c;
     double x[5];
     int y[10];
-    printf("The adress of a variable:%X\n",&a);
-    printf("The adress of c variable:%X\n",&c);
-    printf("The adress of x variable:%X\n",x);
-    printf("The adress of x[0] variable:%X\n",&x[0]);
-    printf("The adress of x[1] variable:%X\n",&x[1]);
+    printf("The address of a variable:%X\n",&a);
+    printf("The address of c variable:%X\n",&c);
+    printf("The address of x variable:%X\n",x);
+    printf("The address of x[0] variable:%X\n",&x[0]);
+    printf("The address of x[1] variable:%X\n",&x[1]);
     for(i=0;i<10;i++)
-        printf("The adress of y[%d] is %x\n",i,&y[i]);
+        printf("The address of y[%d] is %x\n",i,&y[i]);
     return 0;
 }
